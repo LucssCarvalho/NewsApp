aab66d61511ea2c428ac93a97d541bf685e41c4a
LucssCarvalho <lcarvalho1@pagseguro.com>
2022-06-17 16:42:12 -0300

    configurando retrofit

---
+++ app/src/main/java/daniellopes/io/newsappstarter/util/Constants.kt
@@ -0,0 +1,8 @@
+package daniellopes.io.newsappstarter.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "169cab502bec488aa6f8a988c041ac71"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
