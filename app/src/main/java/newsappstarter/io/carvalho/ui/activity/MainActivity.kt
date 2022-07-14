b6543e553cc8271d2a6cc5a1b12c821f499b3d43
LucssCarvalho <lcarvalho1@pagseguro.com>
2022-07-13 21:49:57 -0300

    update navgacao do app

--- app/src/main/java/newsappstarter/io/carvalho/ui/activity/MainActivity.kt
+++ app/src/main/java/newsappstarter/io/carvalho/ui/activity/MainActivity.kt
@@ -1,18 +1,35 @@
 package newsappstarter.io.carvalho.ui.activity
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import newsappstarter.R
 import newsappstarter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initViews(binding)
+    }
+
+    private fun initViews(binding: ActivityMainBinding) {
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        var navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _ , destination, _ ->
+            title = destination.label
+        }
+         binding.bottomNavigation.apply {
+             setupWithNavController(navController = navController)
+             setOnItemReselectedListener {}
+         }
     }
 
 }
