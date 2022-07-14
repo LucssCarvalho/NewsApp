0dc244f3e8ac452fc3652eb78639f389331ea2f2
LucssCarvalho <lcarvalho1@pagseguro.com>
2022-07-13 21:05:24 -0300

    update nav menu

--- app/src/main/java/newsappstarter/io/carvalho/ui/activity/MainActivity.kt
+++ app/src/main/java/newsappstarter/io/carvalho/ui/activity/MainActivity.kt
@@ -1,95 +1,18 @@
 package newsappstarter.io.carvalho.ui.activity
 
-import newsappstarter.io.carvalho.repository.NewsDataSource
-import NewsPresenter
-import ViewHome
-import android.content.Intent
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.Toast
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.viewbinding.ViewBinding
-import newsappstarter.io.carvalho.util.Constants.Companion.ARTICLE_KEY
-import newsappstarter.R
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
 import newsappstarter.databinding.ActivityMainBinding
-import newsappstarter.io.carvalho.ui.adapter.MainAdapter
-import newsappstarter.io.carvalho.data.model.Article
 
-class MainActivity : AbstractActivity(), ViewHome.View {
-
-    private val mainAdapter by lazy {
-        MainAdapter()
-    }
-
-    private lateinit var presenter: NewsPresenter
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    override fun getLayout(): ViewBinding {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        return binding
+        setContentView(binding.root)
     }
 
-    override fun onInject() {
-        val dataSource = NewsDataSource()
-        presenter = NewsPresenter(this, dataSource)
-        presenter.requestAll()
-        configRecycle()
-        clickAdapter()
-    }
-
-    private fun configRecycle() {
-        with(binding.rvNews) {
-            adapter = mainAdapter
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            addItemDecoration(
-                DividerItemDecoration(
-                    this@MainActivity,
-                    DividerItemDecoration.VERTICAL
-                )
-            )
-        }
-    }
-
-    private fun clickAdapter() {
-        mainAdapter.setOnClickListener { article ->
-            val intent = Intent(this, ArticleActivity::class.java)
-            intent.putExtra(ARTICLE_KEY, article)
-            startActivity(intent)
-        }
-    }
-
-    override fun showProgressBar() {
-        binding.rvProgressBar.visibility = View.VISIBLE
-    }
-
-    override fun showFailure(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-    }
-
-    override fun hideProgressBar() {
-        binding.rvProgressBar.visibility = View.INVISIBLE
-    }
-
-    override fun showArticles(articles: List<Article>) {
-        mainAdapter.differ.submitList(articles.toList())
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_item, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.search_menu -> {
-                Intent(this, SearchActivity::class.java).apply {
-                    startActivity(this)
-                }
-            }
-        }
-        return super.onOptionsItemSelected(item)
-    }
 }
